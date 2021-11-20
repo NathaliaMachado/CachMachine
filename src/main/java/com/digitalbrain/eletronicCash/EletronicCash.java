a57540a4279b096abb84ee76d766409f910b86bd
nathaliamachd <nathaliamachd@gmail.com>
2021-11-20 17:13:04 -0300

    NoChangeDeposit

--- src/main/java/com/digitalbrain/eletronicCash/EletronicCash.java
+++ src/main/java/com/digitalbrain/eletronicCash/EletronicCash.java
@@ -3,12 +3,18 @@ package com.digitalbrain.eletronicCash;
 import lombok.Data;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 public class EletronicCash {
 
     private MoneyAmount moneyAmount;
 
     public void initCash(MoneyAmount moneyAmount) {
+        this.moneyAmount = moneyAmount;
+    }
+
+    public void DepositCash(List<MoneyAmount> amount) {
         this.moneyAmount = moneyAmount;
     }
 }
--- src/test/java/com/digitalbrain/eletronicCash/EletronicCashTest.java
+++ src/test/java/com/digitalbrain/eletronicCash/EletronicCashTest.java
@@ -3,15 +3,31 @@ package com.digitalbrain.eletronicCash;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class EletronicCashTest {
+
+    EletronicCash eletronicCash = new EletronicCash();
 
     @Test
     void whenInitCash_havingOneCedule_thenDepositMoney() {
-        EletronicCash eletronicCash = new EletronicCash();
+
         MoneyAmount moneyAmount = new MoneyAmount(Cedule.FIVE, 1);
         eletronicCash.initCash(moneyAmount);
         Assertions.assertEquals(Cedule.FIVE, eletronicCash.getMoneyAmount().getCedule());
         Assertions.assertEquals(1, eletronicCash.getMoneyAmount().getAmount());
     }
 
+    @Test
+    void whenDepositCash_havingMoreThenOneCedule_thenDepositMoney() {
+        List<MoneyAmount> amount = new ArrayList<>();
+        MoneyAmount n1 = new MoneyAmount(Cedule.FIFTY, 2);
+        amount.add(n1);
+        MoneyAmount n2 = new MoneyAmount(Cedule.TEN, 1);
+        amount.add(n2);
+        eletronicCash.DepositCash(amount);
+        Assertions.assertEquals(true, amount.contains(n1));
+        Assertions.assertEquals(true, amount.contains(n2));
+    }
 }
