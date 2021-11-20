f8cabff695feb79d01180c5a6c30cbf10d6748bf
nathaliamachd <nathaliamachd@gmail.com>
2021-11-20 16:50:39 -0300

    first method done

--- src/test/java/com/digitalbrain/eletronicCash/EletronicCashTest.java
+++ src/test/java/com/digitalbrain/eletronicCash/EletronicCashTest.java
@@ -6,11 +6,12 @@ import org.junit.jupiter.api.Test;
 class EletronicCashTest {
 
     @Test
-    void whenInitCach_havingOneCedule_thenDepositMoney() {
+    void whenInitCash_havingOneCedule_thenDepositMoney() {
         EletronicCash eletronicCash = new EletronicCash();
         MoneyAmount moneyAmount = new MoneyAmount(Cedule.FIVE, 1);
         eletronicCash.initCash(moneyAmount);
         Assertions.assertEquals(Cedule.FIVE, eletronicCash.getMoneyAmount().getCedule());
         Assertions.assertEquals(1, eletronicCash.getMoneyAmount().getAmount());
     }
+
 }
